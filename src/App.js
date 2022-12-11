4f74ee42878025e1854f568941ce9b4de6b9c861
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2022-12-11 23:33:38 +0530

    feat: setting route form single user data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import AsyncAwait from "./AsyncAwait";
+import UserInfo from "./UserInfo";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<AsyncAwait />} />
+        {/* Fetch data according to ID */}
+        <Route path="/user/:id" element={<UserInfo />} />
       </Routes>
     </div>
   );
